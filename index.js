a32f8a101ad4020f75f1b0ec2db4aff353406f4b
cleverson-rocha <cleverson.rocha@minu.co>
2023-11-27 10:39:29 -0300

    Atualizacão da rota

--- index.js
+++ index.js
@@ -8,42 +8,42 @@ const randomCel = () => {
     return "55" + ddd.toString() + "9" + parseInt(Math.random() * (80000000 - 99999999) + 99999999).toString();
 };
 const requestMobileNumber = (exOperation) => {
-    // console.log("requestMobileNumber")
-    // console.log(`calling ${process.env.MOBILE_NUMBER_URL}`)
-    const url = `http://172.18.34.90:55113/discovercarriertransform/${randomCel()}/true`;
-    // `http://172.18.34.196:55113/discovercarriertransform/${randomCel()}/true`;
-    // console.log(`calling ${url}`)
-    return new Promise((resolve, reject) => {
-        request(url, exOperation, (error, response, body) => {
-            if (error) {
-                return reject(error);
-            }
+  // console.log("requestMobileNumber")
+  // console.log(`calling ${process.env.MOBILE_NUMBER_URL}`)
+  const url = `http://mobile-number.k8s-hmg.internal/discovercarriertransform/${randomCel()}/true`;
+  // `http://172.18.34.196:55113/discovercarriertransform/${randomCel()}/true`;
+  // console.log(`calling ${url}`)
+  return new Promise((resolve, reject) => {
+    request(url, exOperation, (error, response, body) => {
+      if (error) {
+        return reject(error);
+      }
 
-            const bodyJson = JSON.parse(body);
+      const bodyJson = JSON.parse(body);
 
-            if (!bodyJson.CarrierClientCode) {
-                return reject(exOperation);
-            }
-            if ((exOperation && bodyJson.CarrierClientCode == exOperation) || !exOperation) {
-                resolve(bodyJson);
-            } else {
-                reject(exOperation)
-            }
-        });
+      if (!bodyJson.CarrierClientCode) {
+        return reject(exOperation);
+      }
+      if ((exOperation && bodyJson.CarrierClientCode == exOperation) || !exOperation) {
+        resolve(bodyJson);
+      } else {
+        reject(exOperation);
+      }
     });
+  });
 };
 const getValidNumber = (exOperation) => {
-    return requestMobileNumber(exOperation)
-        .then(bodyData => console.log(`${bodyData.RegionCode};${bodyData.TransformedNumber}`))
-        .catch((exOperation) => getValidNumber(exOperation));
+  return requestMobileNumber(exOperation)
+    .then((bodyData) => console.log(`${bodyData.RegionCode};${bodyData.TransformedNumber}`))
+    .catch((exOperation) => getValidNumber(exOperation));
 };
 
 const getPhoneNumbers = async (qty, exOperation) => {
-    console.log("qyt: ", qty)
-    const promises = Array.from(new Array(qty)).map(() => getValidNumber(exOperation))
-    const result = await Promise.all(promises);
-    return result
+  console.log('qyt: ', qty);
+  const promises = Array.from(new Array(qty)).map(() => getValidNumber(exOperation));
+  const result = await Promise.all(promises);
+  return result;
 };
 
-getPhoneNumbers(1, '1')
+getPhoneNumbers(1, '3');
 
